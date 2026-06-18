98f9fe339420b8458a6a7ec88447326556513d42
ermiya1129-prog <ermiya1129@gmail.com>
2026-06-18 17:33:05 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert("ارمیا جان، app.js اجرا شد!");
+async function loadPrices() {
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether,dogecoin&vs_currencies=usd"
+  );
+
+  const data = await res.json();
+
+  document.getElementById("coins").innerHTML = 
+    <div class="card">USDT: $${data.tether.usd}</div>
+    <div class="card">BTC: $${data.bitcoin.usd}</div>
+    <div class="card">ETH: $${data.ethereum.usd}</div>
+    <div class="card">DOGE: $${data.dogecoin.usd}</div>
+  ;
+}
+
+loadPrices();
